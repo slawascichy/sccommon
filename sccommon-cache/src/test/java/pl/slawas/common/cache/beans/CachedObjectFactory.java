3dc901620665a67887bc517c829e4ced8f00fd50
Sławomir 'slawas' Cichy <scichy@scisoftware.pl>
2018-10-17 16:04:26 +0200

    Poprawienie zależności do schelper'a.

--- sccommon-cache/src/test/java/pl/slawas/common/cache/beans/CachedObjectFactory.java
+++ sccommon-cache/src/test/java/pl/slawas/common/cache/beans/CachedObjectFactory.java
@@ -1,5 +1,6 @@
 package pl.slawas.common.cache.beans;
 
+import java.io.IOException;
 import java.util.Properties;
 
 import pl.slawas.common.cache.CacheSharing;
@@ -14,35 +15,35 @@ public class CachedObjectFactory extends ObjectWithCacheSupport {
 	 * 
 	 */
 	private static final long serialVersionUID = -4795177687749610587L;
-	public static final String cachedObjectRegionName = CachedObjectFactory.class
-			.getSimpleName();
+	public static final String cachedObjectRegionName = CachedObjectFactory.class.getSimpleName();
 
 	protected CachedObjectFactory(CacheUsage cacheUsage, Properties props) {
-		super(cacheUsage, cachedObjectRegionName, CacheSharing.NONE, false,
-				props);
+		super(cacheUsage, cachedObjectRegionName, CacheSharing.NONE, false, props);
 	}
 
-	public static CachedObjectResult get(NameValuePair object,
-			CacheUsage cacheUsage, Properties props) throws CacheErrorException {
+	public static CachedObjectResult get(NameValuePair object, CacheUsage cacheUsage, Properties props)
+			throws CacheErrorException {
 
-		CachedObjectFactory obj = new CachedObjectFactory(cacheUsage, props);
-		String key = object.getName();
-		Object cachedObject = obj.getCache().get(key);
-		if (cachedObject != null && cachedObject instanceof NameValuePair) {
-			switch (cacheUsage) {
-			case TO_REMOVE:
-				obj.getCache().remove(key);
-				return new CachedObjectResult(null, true);
-			case REFRESH:
+		try (CachedObjectFactory obj = new CachedObjectFactory(cacheUsage, props);) {
+			String key = object.getName();
+			Object cachedObject = obj.getCache().get(key);
+			if (cachedObject != null && cachedObject instanceof NameValuePair) {
+				switch (cacheUsage) {
+				case TO_REMOVE:
+					obj.getCache().remove(key);
+					return new CachedObjectResult(null, true);
+				case REFRESH:
+					obj.getCache().put(key, object);
+					return new CachedObjectResult(object, false);
+				default:
+					return new CachedObjectResult((NameValuePair) cachedObject, true);
+				}
+			} else {
 				obj.getCache().put(key, object);
 				return new CachedObjectResult(object, false);
-			default:
-				return new CachedObjectResult((NameValuePair) cachedObject,
-						true);
 			}
-		} else {
-			obj.getCache().put(key, object);
-			return new CachedObjectResult(object, false);
+		} catch (IOException e) {
+			throw new CacheErrorException(e);
 		}
 	}
 
